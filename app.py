47d5a8d2f28325f9ae6b5d09f4775896114d2f0b
Rusich <vasenin@verqor.com>
2025-05-17 21:16:29 +0300

    added form.html

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import mimetypes
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from requests_toolbelt.multipart import decoder
 from PIL import Image
@@ -10,13 +11,32 @@ ALLOWED_EXT = {'jpg', 'jpeg', 'png', 'gif'}
 MAX_FILE_SIZE = 5 * 1024 * 1024
 
 os.makedirs(UPLOAD_DIR, exist_ok=True)
+files = {'/': './static/index.html', '/upload': './static/form.html'}
 
 class API(SimpleHTTPRequestHandler):
     def do_GET(self):
-        if self.path == '/':
-            print('GET home')
-        if self.path == '/images':
+        print(self.path)
+        if self.path in ['/', '/upload']:
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            with open(files[self.path], 'rb') as f:
+                self.wfile.write(f.read())
+        elif self.path.startswith('/css/') or self.path.startswith('/js/') or self.path.startswith('/img/'):
+            file_path = './static' + self.path
+            if os.path.exists(file_path) and os.path.isfile(file_path):
+                self.send_response(200)
+                mime_type, _ = mimetypes.guess_type(file_path)
+                self.send_header('Content-type', mime_type or 'application/octet-stream')
+                self.end_headers()
+                with open(file_path, 'rb') as f:
+                    self.wfile.write(f.read())
+        elif self.path == '/images':
             print('GET list')
+        else:
+            self.send_response(404)
+            self.end_headers()
+            self.wfile.write(b'404 Not Found')
     def do_POST(self):
         if self.path == '/upload':
             print('POST upload')
